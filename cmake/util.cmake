4762b4bb573f699dc0f1e1f16a887f9b39b8c99f
Dmitry Baryshnikov <bishop.dev@gmail.com>
2019-06-10 19:40:05 +0300

    Version 2.9

---
+++ cmake/util.cmake
@@ -0,0 +1,169 @@
+################################################################################
+# Project:  LITTLECMS2
+# Purpose:  CMake build scripts
+# Author:   Dmitry Baryshnikov, polimax@mail.ru
+################################################################################
+# Copyright (C) 2015-2019, NextGIS <info@nextgis.com>
+# Copyright (C) 2012,2013,2014-2019 Dmitry Baryshnikov
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+################################################################################
+
+
+function(check_version major minor rev)
+
+    # parse the version number from gdal_version.h and include in
+    # major, minor and rev parameters
+    set(VERSION_FILE ${CMAKE_CURRENT_SOURCE_DIR}/include/lcms2.h)
+
+    file(READ ${VERSION_FILE} _VERSION_H_CONTENTS)
+
+    string(REGEX MATCH "LCMS_VERSION[ \t]+([0-9]+)"
+      _VERSION ${_VERSION_H_CONTENTS})
+    string (REGEX MATCH "([0-9]+)"
+      _VERSION ${_VERSION})
+
+    math(EXPR MAJOR_VERSION "${_VERSION} / 1000")
+    math(EXPR MINOR_VERSION "(${_VERSION} - ${MAJOR_VERSION} * 1000) / 10")
+    math(EXPR REV_VERSION "${_VERSION} - ${MAJOR_VERSION} * 1000 - ${MINOR_VERSION} * 10")
+
+    set(${major} ${MAJOR_VERSION} PARENT_SCOPE)
+    set(${minor} ${MINOR_VERSION} PARENT_SCOPE)
+    set(${rev} ${REV_VERSION} PARENT_SCOPE)
+
+    # Store version string in file for installer needs
+    file(TIMESTAMP ${VERSION_FILE} VERSION_DATETIME "%Y-%m-%d %H:%M:%S" UTC)
+    set(VERSION ${MAJOR_VERSION}.${MINOR_VERSION}.${REV_VERSION})
+    get_cpack_filename(${VERSION} PROJECT_CPACK_FILENAME)
+    file(WRITE ${CMAKE_BINARY_DIR}/version.str "${VERSION}\n${VERSION_DATETIME}\n${PROJECT_CPACK_FILENAME}")
+
+endfunction(check_version)
+
+function(set_libraries libs is_shared bld_dir release_name debug_name)
+    if (MSVC)
+        if(is_shared)
+            set(${libs}
+                    debug "${bld_dir}/Debug/${CMAKE_SHARED_LIBRARY_PREFIX}${debug_name}${CMAKE_STATIC_LIBRARY_SUFFIX}"
+                    optimized "${bld_dir}/Release/${CMAKE_SHARED_LIBRARY_PREFIX}${release_name}${CMAKE_STATIC_LIBRARY_SUFFIX}"
+                PARENT_SCOPE)
+        else()
+            set(${libs}
+                    debug"${bld_dir}/Debug/${CMAKE_STATIC_LIBRARY_PREFIX}${debug_name}${CMAKE_STATIC_LIBRARY_SUFFIX}"
+                    optimized "${bld_dir}/Release/${CMAKE_STATIC_LIBRARY_PREFIX}${release_name}${CMAKE_STATIC_LIBRARY_SUFFIX}"
+                PARENT_SCOPE)
+        endif()
+    else()
+        if(is_shared)
+            set(${libs}
+                "${bld_dir}/${CMAKE_SHARED_LIBRARY_PREFIX}${debug_name}${CMAKE_SHARED_LIBRARY_SUFFIX}"
+            PARENT_SCOPE)
+        else()
+            set(${libs}
+                "${bld_dir}/${CMAKE_STATIC_LIBRARY_PREFIX}${release_name}${CMAKE_STATIC_LIBRARY_SUFFIX}"
+            PARENT_SCOPE)
+        endif()
+    endif()
+endfunction(set_libraries)
+
+function(report_version name ver)
+
+    string(ASCII 27 Esc)
+    set(BoldYellow  "${Esc}[1;33m")
+    set(ColourReset "${Esc}[m")
+
+    message("${BoldYellow}${name} version ${ver}${ColourReset}")
+
+endfunction()
+
+function(warning_msg text)
+    if(NOT SUPPRESS_VERBOSE_OUTPUT)
+    string(ASCII 27 Esc)
+    set(Red         "${Esc}[31m")
+    set(ColourReset "${Esc}[m")
+
+    message(STATUS "${Red}${text}${ColourReset}")
+    endif()
+endfunction()
+
+
+# macro to find packages on the host OS
+macro( find_exthost_package )
+    if(CMAKE_CROSSCOMPILING)
+        set( CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER )
+        set( CMAKE_FIND_ROOT_PATH_MODE_LIBRARY NEVER )
+        set( CMAKE_FIND_ROOT_PATH_MODE_INCLUDE NEVER )
+
+        find_package( ${ARGN} )
+
+        set( CMAKE_FIND_ROOT_PATH_MODE_PROGRAM ONLY )
+        set( CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY )
+        set( CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY )
+    else()
+        find_package( ${ARGN} )
+    endif()
+endmacro()
+
+
+# macro to find programs on the host OS
+macro( find_exthost_program )
+    if(CMAKE_CROSSCOMPILING)
+        set( CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER )
+        set( CMAKE_FIND_ROOT_PATH_MODE_LIBRARY NEVER )
+        set( CMAKE_FIND_ROOT_PATH_MODE_INCLUDE NEVER )
+
+        find_program( ${ARGN} )
+
+        set( CMAKE_FIND_ROOT_PATH_MODE_PROGRAM ONLY )
+        set( CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY )
+        set( CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY )
+    else()
+        find_program( ${ARGN} )
+    endif()
+endmacro()
+
+
+function(get_cpack_filename ver name)
+    get_compiler_version(COMPILER)
+    if(BUILD_STATIC_LIBS)
+        set(STATIC_PREFIX "static-")
+    endif()
+
+    set(${name} ${PROJECT_NAME}-${STATIC_PREFIX}${ver}-${COMPILER} PARENT_SCOPE)
+endfunction()
+
+function(get_compiler_version ver)
+    ## Limit compiler version to 2 or 1 digits
+    string(REPLACE "." ";" VERSION_LIST ${CMAKE_C_COMPILER_VERSION})
+    list(LENGTH VERSION_LIST VERSION_LIST_LEN)
+    if(VERSION_LIST_LEN GREATER 2 OR VERSION_LIST_LEN EQUAL 2)
+        list(GET VERSION_LIST 0 COMPILER_VERSION_MAJOR)
+        list(GET VERSION_LIST 1 COMPILER_VERSION_MINOR)
+        set(COMPILER ${CMAKE_C_COMPILER_ID}-${COMPILER_VERSION_MAJOR}.${COMPILER_VERSION_MINOR})
+    else()
+        set(COMPILER ${CMAKE_C_COMPILER_ID}-${CMAKE_C_COMPILER_VERSION})
+    endif()
+
+    if(WIN32)
+        if(CMAKE_CL_64)
+            set(COMPILER "${COMPILER}-64bit")
+        endif()
+    endif()
+
+    set(${ver} ${COMPILER} PARENT_SCOPE)
+endfunction()
